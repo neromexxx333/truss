3535b0b4cd77db75472bfef6bb3b92817caefb05
neromexxx333 <neromexxx333@gmail.com>
2026-03-14 08:22:53 +0800

    truss_streamlit.py

--- truss_streamlit.py
+++ truss_streamlit.py
@@ -22,7 +22,7 @@ unsafe_allow_html=True
 )
 
 st.markdown(
-"<h5 style='text-align:left'>====================================================</h5>",
+"<h5 style='text-align:left'>==========================================================</h5>",
 unsafe_allow_html=True
 )
 
